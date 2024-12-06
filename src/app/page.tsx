6c570c06a35d207e3abda2d2d5210b506e6c0540
Praveen Joshua <praveenjoshua2394@gmail.com>
2024-12-07 01:43:24 +0530

    changes in layout and page ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
-import { AuthProvider } from '@/contexts/AuthContext';
 import BibleApp from "./BibleApp"
 
 export default function Page() {
   return (
-    <AuthProvider>
+    <main>
       <BibleApp />
-    </AuthProvider>
+    </main>
   );
 }
